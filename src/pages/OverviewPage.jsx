e28dc0c3679056e0bc756c3c3fa3f54ec2e9e99d
Ozhan <ozhanyt@gmail.com>
2025-12-10 15:55:18 +0300

    fix: Add null check to second funds.map() call

--- src/pages/OverviewPage.jsx
+++ src/pages/OverviewPage.jsx
@@ -442,7 +442,7 @@ export function OverviewPage({ isDarkMode, setIsDarkMode }) {
                         strategy={rectSortingStrategy}
                     >
                         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                            {funds.map((fund) => {
+                            {(funds || []).map((fund) => {
                                 return (
                                     <SortableFundCard
                                         key={fund.id}
